d4ba43331d215397574954b1335c038910e64bd1
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-09-06 12:04:21 -0300

    feat: replacing docker hub with ACR to use the image

--- PeopleRegistration.Api/Program.cs
+++ PeopleRegistration.Api/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/", () => "Hello World ok!");
+app.MapGet("/", () => "Imagem Do ACR");
 
 app.MapGet("/people", () => "Novo endpoint para teste do deploy automático");
 
